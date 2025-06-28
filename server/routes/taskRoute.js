4feb3a152c7955b3587e7c873d7c656b553a548b
saurabhjaykar1603 <jaykarsaurabh97@gmail.con>
2025-06-28 18:10:02 +0530

    refactor: update task routes with explicit action-based paths

--- server/routes/taskRoute.js
+++ server/routes/taskRoute.js
@@ -1,22 +1,22 @@
 import { Router } from "express";
 import {
-    createTask,
-    deleteTaskById,
-    getTaskById,
-    getTasks,
-    updateTaskById,
+  createTask,
+  deleteTaskById,
+  getTaskById,
+  getTasks,
+  updateTaskById,
 } from "../controllers/taskController.js";
 import authMiddleware from "../middleware/auth.js";
 
 const taskRouter = Router();
 
 // Route for getting all tasks and creating a new task
-taskRouter.get("/", authMiddleware, getTasks);
-taskRouter.post("/", authMiddleware, createTask);
+taskRouter.get("/get", authMiddleware, getTasks);
+taskRouter.post("/create", authMiddleware, createTask);
 
 // Routes for operations on specific tasks by ID
-taskRouter.get("/:id", authMiddleware, getTaskById);
-taskRouter.put("/:id", authMiddleware, updateTaskById);
-taskRouter.delete("/:id", authMiddleware, deleteTaskById);
+taskRouter.get("/get/:id", authMiddleware, getTaskById);
+taskRouter.put("/update/:id", authMiddleware, updateTaskById);
+taskRouter.delete("/delete/:id", authMiddleware, deleteTaskById);
 
 export default taskRouter;
