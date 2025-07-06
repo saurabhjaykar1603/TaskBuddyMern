c0001ec178b4e146d2a7e17e37211deeb0546644
saurabhjaykar1603 <jaykarsaurabh97@gmail.con>
2025-07-06 18:04:13 +0530

    ref : change health message routes

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use("/api/v1/tasks", taskRouter);
 
 
 app.get('/', (req, res) => {
-    res.send("Hello World");
+    res.send("Server is running successfully, API is ready to use");
 });
 
 app.listen(port, () => {
