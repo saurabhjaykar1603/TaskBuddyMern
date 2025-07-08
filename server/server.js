5c4a8f8c9dacdcdebb82dcaf1bf5a484268266a7
saurabhjaykar1603 <jaykarsaurabh97@gmail.con>
2025-07-08 22:51:59 +0530

    added corse

--- server/server.js
+++ server/server.js
@@ -11,7 +11,10 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 //middleware
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
